60dfa6ab47ae2d6e648970e0c7392fdb365e14b5
Kashish Sharma <76687274+kashish2610@users.noreply.github.com>
2025-06-23 00:07:32 +0530

    Add files via upload

---
+++ Anime1.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+movies_df =pickle.load(open('Anime_list.pkl','rb'))
+#similarity=pickle.load(open('similarity1.pkl','rb'))
+import requests
+
+url = "https://drive.google.com/file/d/1VNnKhgB0xNdVq4qL6HbaWvAXIT0h1Gsb/view?usp=sharing"
+response = requests.get(url)
+with open("similarity1.pkl", "rb") as similarity:
+     similarity= pickle.load(similarity)
+movies_title= movies_df['name'].values
+def recommend(movie):
+    movie_index = movies_df[movies_df['name'] == movie].index[0]
+    distance = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    for i in movie_list:
+        movie_id=i[0]
+        # fetch poster
+        recommended_movies.append(movies_df.iloc[i[0]]['name'])
+    return recommended_movies
+st.title('Anime Recommender')
+Selected_movie = st.selectbox(
+"Select a Anime",
+movies_title
+)
+if st.button("Recommend", type="tertiary"):
+    recommendations = recommend(Selected_movie)
+    for i in recommendations:
+        st.write(i)
